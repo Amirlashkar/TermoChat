55edee51ae81774914288c4522367e50430d5263
Goshtasp <amireza1422@gmail.com>
2025-02-13 23:48:07 +0330

    FEAT: Loading environment variables

---
+++ config/env.go
@@ -0,0 +1,32 @@
+package config
+
+
+import (
+  "os"
+  "log"
+  "github.com/joho/godotenv"
+)
+
+
+type Env struct {
+  SECRET  string
+  PORT    string
+  DB_NAME string
+  DB_USER string
+  DB_PASS string
+}
+
+func LoadEnv() Env {
+  err := godotenv.Load()
+  if err != nil {
+    log.Println("Warning: No .env file found, using system environment variables")
+  }
+
+  return Env {
+    SECRET:   os.Getenv("SECRET_KEY"),
+    PORT:     os.Getenv("PORT"),
+    DB_NAME:  os.Getenv("DB_NAME"),
+    DB_USER:  os.Getenv("DB_USER"),
+    DB_PASS:  os.Getenv("DB_PASS"),
+  }
+}
